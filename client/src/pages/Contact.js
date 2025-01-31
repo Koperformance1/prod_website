cd07d01f45c6288a6781a1d393ae4be51583c0ab
Adam Crowe <awcrowe00@gmail.com>
2025-01-31 15:23:48 -0800

    removing localhost specification from contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -16,7 +16,7 @@ function Contact() {
         setStatus('sending');
 
         try {
-            const response = await fetch('http://localhost:5001/api/contact', {
+            const response = await fetch('/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
